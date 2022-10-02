2317aae23991cdab8124705938813a818909af70
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-10-02 14:50:43 +0300

    Not finished security configuration using a new technique (maybe I'll come back to it later). In this moment, the standard approach will be used

--- src/main/java/com/chernikov/registerme/security/config/WebSecurityConfig.java
+++ src/main/java/com/chernikov/registerme/security/config/WebSecurityConfig.java
@@ -1,13 +1,38 @@
 package com.chernikov.registerme.security.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests((auth) -> {
+                            try {
+                                auth
+                                        .antMatchers("/api/v*/registration/**").permitAll()
+                                        .anyRequest().authenticated()
+                                        .and()
+                                        .formLogin();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                ).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
 
 }
