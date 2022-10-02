77d71f935276cef33fd70fef6ba1a3a2df8ccbb2
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-10-02 15:01:15 +0300

    Security Configured (WebSecurityConfig, PasswordEncoder)

--- src/main/java/com/chernikov/registerme/security/config/WebSecurityConfig.java
+++ src/main/java/com/chernikov/registerme/security/config/WebSecurityConfig.java
@@ -1,38 +1,46 @@
 package com.chernikov.registerme.security.config;
 
+import com.chernikov.registerme.appuser.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserService userService;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
                 .csrf().disable()
-                .authorizeHttpRequests((auth) -> {
-                            try {
-                                auth
-                                        .antMatchers("/api/v*/registration/**").permitAll()
-                                        .anyRequest().authenticated()
-                                        .and()
-                                        .formLogin();
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                ).httpBasic(Customizer.withDefaults());
-
-        return httpSecurity.build();
+                .authorizeRequests()
+                    .antMatchers("/api/v*/registration/**")
+                    .permitAll()
+                .anyRequest()
+                .authenticated()
+                    .and()
+                .formLogin();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
 
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
 }
